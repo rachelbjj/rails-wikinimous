ed7c0ef7e32a5b1af5d61ef905a9b00fdae3bf8b
rachel BJORK <rachel.bjork@edhec.com>
2016-08-09 17:51:01 +0200

    rails new

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,41 @@
+class ArticleController < ApplicationController
+  def show
+    @articles = Article.all
+  end
+
+  def delete
+    @article.delete
+    redirect_to articles_path
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article, method: :get)
+  end
+
+  def edit
+     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article, method: :get)
+  end
+
+  def delete
+    @article = Article.find(params[:id])
+    @article.delete
+    redirect_to articles_path
+  end
+
+  private
+   def article_params
+     params.require(:article).permit(:title, :content)
+   end
+end
